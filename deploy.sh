e7683ea1d4ef4f1d852caf27a73eced31b29ee29
Andrey Vakarev <avakarev@gmail.com>
2012-10-29 00:41:08 +0200

    Smarter version of link function: check for existence of target file and ask for a user action

--- deploy.sh
+++ deploy.sh
@@ -31,15 +31,60 @@ usage(){
 
 link(){
   for i in $1 ; do
-    if [ -e "${PWD}/$i" ]; then
-      echo "item exists"
-      if [ -d "${PWD}/$i" ]; then
-        if [ -d "${HOME}/.$i" ]; then
-          rm -rf "${HOME}/.$i"
-        fi;
-      fi;
-      echo "link it again"
-      ln -Ffs "${PWD}/$i" "${HOME}/.$i"
+
+    # symlink source file/dir
+    this_file_path="${PWD}/$i"
+
+    # symlink target file/dir
+    that_file_path="${HOME}/.$i"
+
+    # is it already linked?
+    that_link_path=$(readlink "$that_file_path")
+
+    # if source file/dir is exists
+    if [ -e "$this_file_path" ]; then
+        # if target file/dir exists
+        if [ -e "$that_file_path" ]; then
+            # if target file/dir is symlink and already linked to source file/dir
+            if [ "$that_link_path" == "$this_file_path" ]; then
+                # do nothing
+                echo "ignoring: ./$i is already linked to $this_file_path"
+            # else - ask how to handle that existing file/dir
+            else
+                echo "Existing file/dir $that_file_path will be overitten by symlink to $this_file_path"
+                echo "  Select an action:"
+                echo "  1. ignore and do nothing"
+                echo "  2. backup $that_file_path to $that_file_path.orig and create symlink to $that_file_path"
+                echo "  3. remove $that_file_path and create symlink to $that_file_path"
+                read -p "[1]: " user_input
+
+                # if user has chosen backup or remove
+                if [ "$user_input" == '2' ] || [ "$user_input" == '3' ]; then
+                    # backup
+                    if [ "$user_input" == '2' ]; then
+                        echo "backing up: $that_file_path to $that_file_path.orig"
+                        mv "$that_file_path" "$that_file_path.orig"
+                    # remove
+                    else
+                        echo "removing: $that_file_path"
+                        rm -rf "$that_file_path"
+                    fi
+
+                    echo "linking: $that_file_path to $this_file_path"
+                    ln -Ffs "$this_file_path" "$that_file_path"
+                # user has chosen ignore and to nothing
+                else
+                    echo "ignoring: do nothing with $that_file_path"
+                fi
+            fi
+        # target file/dir does not exsits, create an symlink
+        else
+            echo "linking: $that_file_path to $this_file_path"
+            ln -Ffs "$this_file_path" "$that_file_path"
+        fi
+    # source file/dir does not exist, do nothing
+    else
+        echo "ignoring: $that_file_path does'nt exists"
     fi
   done
 }
@@ -56,7 +101,7 @@ case "$1" in
         if [ -z "$bundle" ]; then
             echo "it is not clear how to link [$bundle_name]"
         else
-            echo "[$bundle_name] being linked successfuly: $bundle"
+            echo "*** [$bundle_name] consists of: $bundle ***"
             link "$bundle"
         fi
     done
